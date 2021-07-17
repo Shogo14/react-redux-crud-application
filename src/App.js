5c9c2d46f776b2d2aa8c04915adcf70477467762
sho14kim <shogo14kinjo@gmail.com>
2021-07-17 21:08:59 +0900

    prop-typeの学習

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
   const profiles = [
@@ -12,6 +13,7 @@ const App = () => {
     },
     {
       name: "NoName",
+      age: 3,
     },
   ];
   return (
@@ -31,8 +33,8 @@ const User = (props) => {
   );
 };
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 };
-
 export default App;
